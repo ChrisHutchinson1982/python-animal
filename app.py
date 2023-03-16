b01b9ec6f1f0cd92f2ca799dfe55b90875a9ae36
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-16 14:05:08 +0000

    Added /request/<id> GET endpoint into app.py to get animal by id and is working in PostMan

--- app.py
+++ app.py
@@ -6,41 +6,25 @@ from models import Animal
 
 app = Flask(__name__)
 
-# create the database and table. Insert 10 test animals into db
+# create the database and table. Insert 6 test animals into db
 # Do this only once to avoid inserting the test animals into 
-# the db multiple times
+# the da multiple times
 if not os.path.isfile('animalsTest.db'):
     db.connect()
 
-  
+# test route
+@app.route('/', methods=['GET'])
+def index():
+    '''Index'''
+    return jsonify({ 
+        'success': True,
+        'message': 'our API works'
+    })
 
-# route for landing page
-# check out the template folder for the index.html file
-# check out the static folder for css and js files
-
-
-# @app.route("/")
-# def index():
-#     return 'Hello Animals'
-
-# # def isValid(email):
-# #     regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
-# #     if re.fullmatch(regex, email):
-# #       return True
-# #     else:
-# #       return False
-
-
+# route to create a new animal
 @app.route("/request", methods=['POST'])
 def postRequest():
     req_data = request.get_json()
-    # email = req_data['email']
-    # if not isValid(email):
-    #     return jsonify({
-    #         'status': '422',
-    #         'res': 'failure',
-    #         'error': 'Invalid email format. Please enter a valid email address'
-    #     })
     name = req_data['name']
     food = req_data['food']
     animals = [a.serialize() for a in db.view()]
@@ -65,7 +49,7 @@ def postRequest():
                 'msg': 'Success creating a new animal!👍😀'
             })
 
-
+# route to get all animals
 @app.route('/request', methods=['GET'])
 def getRequest():
     content_type = request.headers.get('Content-Type')
@@ -94,34 +78,34 @@ def getRequest():
                     'no_of_animals': len(animals)
                 })
 
-
-# @app.route('/request/<id>', methods=['GET'])
-# def getRequestId(id):
-#     req_args = request.view_args
-#     # print('req_args: ', req_args)
-#     animals = [b.serialize() for b in db.view()]
-#     if req_args:
-#         for b in animals:
-#             if b['id'] == int(req_args['id']):
-#                 return jsonify({
-#                     # 'error': '',
-#                     'res': b,
-#                     'status': '200',
-#                     'msg': 'Success getting animal by ID!👍😀'
-#                 })
-#         return jsonify({
-#             'error': f"Error ⛔❌! animal with id '{req_args['id']}' was not found!",
-#             'res': '',
-#             'status': '404'
-#         })
-#     else:
-#         return jsonify({
-#                     # 'error': '',
-#                     'res': animals,
-#                     'status': '200',
-#                     'msg': 'Success getting animal by ID!👍😀',
-#                     'no_of_animals': len(animals)
-#                 })
+# route to get a animal by id
+@app.route('/request/<id>', methods=['GET'])
+def getRequestId(id):
+    req_args = request.view_args
+    # print('req_args: ', req_args)
+    animals = [a.serialize() for a in db.view()]
+    if req_args:
+        for a in animals:
+            if a['id'] == int(req_args['id']):
+                return jsonify({
+                    # 'error': '',
+                    'res': a,
+                    'status': '200',
+                    'msg': 'Success getting animal by ID!'
+                })
+        return jsonify({
+            'error': f"Error! animal with id '{req_args['id']}' was not found!",
+            'res': '',
+            'status': '404'
+        })
+    else:
+        return jsonify({
+                    # 'error': '',
+                    'res': animals,
+                    'status': '200',
+                    'msg': 'Success getting animal by ID!',
+                    'no_of_animals': len(animals)
+                })
 
 # @app.route("/request", methods=['PUT'])
 # def putRequest():
@@ -129,8 +113,8 @@ def getRequest():
 #     availability = req_data['available']
 #     name = req_data['name']
 #     the_id = req_data['id']
-#     animals = [b.serialize() for b in db.view()]
-#     for b in animals:
+#     animals = [a.serialize() for a in db.view()]
+#     for a in animals:
 #         if b['id'] == the_id:
 #             animal = Animal(
 #                 the_id, 
@@ -140,7 +124,7 @@ def getRequest():
 #             )
 #             print('new animal: ', animal.serialize())
 #             db.update(animal)
-#             new_animals = [b.serialize() for b in db.view()]
+#             new_animals = [a.serialize() for a in db.view()]
 #             print('animals in lib: ', new_animals)
 #             return jsonify({
 #                 # 'error': '',
@@ -161,12 +145,12 @@ def getRequest():
 # def deleteRequest(id):
 #     req_args = request.view_args
 #     print('req_args: ', req_args)
-#     animals = [b.serialize() for b in db.view()]
+#     animals = [a.serialize() for a in db.view()]
 #     if req_args:
-#         for b in animals:
+#         for a in animals:
 #             if b['id'] == int(req_args['id']):
 #                 db.delete(b['id'])
-#                 updated_animals = [b.serialize() for b in db.view()]
+#                 updated_animals = [a.serialize() for a in db.view()]
 #                 print('updated_animals: ', updated_animals)
 #                 return jsonify({
 #                     'res': updated_animals,
